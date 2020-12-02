f01a6d00cda7245cdf4e49c243754565ea9efcfe
Abdullah Hejazi <a.m.a.hejazi@hotmail.com>
2020-12-02 19:17:36 +0200

    Added the basic structure and function

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,84 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="bg-blur">
+    <h1 class="title">Rage Quit</h1>
+    <p class="slogan">If you're gonna be toxic, do it right !</p>
+
+    <RageBox />
+  </div>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import RageBox from './components/RageBox.vue';
 
 export default {
   name: 'App',
+
   components: {
-    HelloWorld
+    RageBox
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @font-face {
+    font-family: RosarivoRegular;
+    src: url(assets/Rosarivo-Regular.ttf);
+  }
+
+  html { 
+    background: url(assets/background.jpg) no-repeat center center fixed; 
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover;
+    background-color: #0f0f0f;
+    color: white;
+
+    filter: progid:DXImageTransform.Microsoft.AlphaImageLoader(src='./assets/background.jpg', sizingMethod='scale');
+    -ms-filter: "progid:DXImageTransform.Microsoft.AlphaImageLoader(src='assets/background.jpg', sizingMethod='scale')";
+  }
+
+  body {
+    margin: 0px;
+    padding: 0px;
+  }
+
+  .bg-blur {
+    position: fixed;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    overflow: auto;
+    background-color: rgb(0,0,0);
+    background-color: rgba(0,0,0,0.5);
+  }
+
+  .main {
+    z-index: 5;
+  }
+
+  #app {
+    font-family: RosarivoRegular, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  .title {
+    text-align: center;
+    font-size: 60px;
+    margin: 50px auto 0px auto;
+  }
+
+  .slogan {
+    text-align: center;
+    margin: 10px auto;
+  }
+
+  @media only screen and (max-width: 400px) {
+    .title {
+      font-size: 35px;
+    }
+  }
 </style>
---
+++ src/components/RageBox.vue
@@ -0,0 +1,208 @@
+<template>
+  <div class="rage-box">
+    <input class="input" placeholder="Type something (e.g EZ JG)" v-model="input" @input="InputChanged(input)" />
+    
+		<p class="warning"> {{ warning }}</p>
+
+		<p class="display" title="This is how it will look lik in the LoL chat !" v-html="display"></p>
+		<img src="../assets/copy.png" class="copy" @click="CopyText" title="Copy Text">
+		<p class="note">This is how the text will show in the chat.</p>
+		<p class="note">Please notice that this only works in the lobby chat, and doesn't work in-game !</p>
+    
+		<img src="../assets/copy.png" class="copy2" @click="CopyText" title="Copy Text">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RageBox',
+
+  data: () => {
+    return {
+      input: '',
+      display: '',
+      warning: '',
+      letters: {
+        'A': '█▀▀█|█▀▀█|▀░░▀',
+        'B': '█▀▀█|█▀▀█|▀▀▀▀',
+        'C': '█▀▀▀|█░░░|▀▀▀▀',
+        'D': '█▀▀░|█░░█|▀▀▀░',
+        'E': '█▀▀|█▀▀|▀▀▀',
+        'F': '█▀▀|█▀▀|▀░░',
+        'G': '█▀▀▀|█░▀█|▀▀▀▀',
+        'H': '█░░█|█▀▀█|▀░░▀',
+        'I': '▀█▀|░█░|▀▀▀',
+        'J': '▀█▀|░█░|▀▀░',
+        'K': '█░█|█▀░|▀░▀',
+        'L': '█░░░|█░░░|▀▀▀▀',
+        'M': '█▀█▀█|█░█░█|▀░▀░▀',
+        'N': '█▀▀█|█░░█|▀░░▀',
+        'O': '█▀▀█|█░░█|▀▀▀▀',
+        'P': '█▀▀█|█▀▀▀|▀░░░',
+        'Q': '█▀▀█░|█░░█░|▀▀▀▀▀',
+        'R': '█▀▀█|█▀▀▀|▀░░▀',
+        'S': '█▀▀▀|▀▀▀█|▀▀▀▀',
+        'T': '▀▀█▀▀|░░█░░|░░▀░░',
+        'U': '█░░█|█░░█|▀▀▀▀',
+        'V': '█░░█|▀░░▀|░▀▀░',
+        'W': '█░█░█|█░█░█|▀▀▀▀▀',
+        'X': '█░░█|░▀▀░|▀░░▀',
+        'Y': '█░█|▀█▀|░▀░',
+        'Z': '▀▀█|▄▀░|▀▀▀',
+        ' ': '░|░|░',
+        '!': '█|▀|▀',
+        '0': '█▀█|█░█|▀▀▀',
+        '1': '▀█|░█|░▀',
+        '2': '▀▀▀█|█▀▀▀|▀▀▀▀',
+        '3': '▀▀█|▀▀█|▀▀▀',
+        '4': '█░░█|▀▀▀█|░░░▀',
+        '5': '█▀▀▀|▀▀▀█|▀▀▀▀',
+        '6': '█░░░|█▀▀█|▀▀▀▀',
+        '7': '▀▀█|░░█|░░▀',
+        '8': '█▀▀█|█▀▀█|▀▀▀▀',
+        '9': '█▀▀█|▀▀▀█|▀▀▀▀'
+      }
+    }
+  },
+
+  mounted: function () {
+    this.InputChanged('EZ JG')
+  },
+
+  methods: {
+    InputChanged: function (input) {
+      var text = input.toUpperCase()
+
+      var lines = [
+        '░',
+        '░',
+        '░'
+      ];
+
+      for (var i = 0; i < text.length; i++) {
+        var letter = text.charAt(i)
+
+        if (!(letter in this.letters)) {
+          this.warning = "One or more characters are not supported !"
+          return false
+        }
+
+        var l = this.letters[letter].split('|')
+
+        lines[0] += l[0] + '░'
+        lines[1] += l[1] + '░'
+        lines[2] += l[2] + '░'
+      }
+
+      if (lines[0].length > 31) {
+        this.warning = 'The number of characters is too much for the LoL Chat size !!'
+      } else {
+        this.warning = ''
+      }
+
+      var emptyLine = '░'.repeat(lines[0].length)
+
+
+      this.display = emptyLine + '<br>' + lines[0] + '<br>' + lines[1] + '<br>' + lines[2] + '<br>' + emptyLine
+    },
+
+    CopyText: function () {
+      var input = document.createElement('textarea');
+      var brRegex = /<br\s*[/]?>/gi;
+      input.innerHTML = ".\r\n" + this.display.replace(brRegex, "\r\n")
+
+      document.body.appendChild(input);
+
+      input.select();
+
+      document.execCommand('copy');
+      document.body.removeChild(input);
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .rage-box {
+    text-align: center;
+  }
+
+  .input {
+    display: block;
+    margin: 50px auto 0px auto;
+    width: 380px;
+    height: 40px;
+    background-color: rgba(0, 0, 0, 0.5);
+    border: 1px solid #665426;
+    color: white;
+    font-size: 20px;
+    padding: 3px 15px 3px 15px;
+    text-align: center;
+  }
+
+  .input:focus {
+    outline: none;
+    border: 1px solid #a78739;
+  }
+
+  .display {
+    padding: 20px;
+    color: white;
+    display: block;
+    width: 350px;
+    height: 160px;
+    margin: 20px auto;
+    background-color: rgba(0, 0, 0, 0.5);
+    border: 1px solid #665426;
+    text-align: center;
+  }
+
+  .copy {
+    display: inline-block;
+    width: 32px;
+    position: relative;
+    margin-top: 0px;
+    top: -65px;
+    left: 165px;
+    cursor: pointer;
+  }
+
+  .copy2 {
+    display: none;
+    width: 32px;
+    margin-top: 0px;
+    cursor: pointer;
+  }
+
+  .warning {
+    color: rgb(255, 91, 91);
+    font-size: 16px;
+    text-align: center;
+    margin-top: 5px;
+  }
+
+  .note {
+    color: #e9bf57;
+    margin: 5px;
+  }
+
+  
+  @media only screen and (max-width: 450px) {
+    .copy {
+      display: none;
+    }
+
+    .input {
+      width: 80%;
+    }
+
+    .display {
+      width: 80%;
+    }
+
+    .copy2 {
+      display: inline-block;
+    }
+  }
+
+</style>
